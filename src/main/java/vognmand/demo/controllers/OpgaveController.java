b51bfb0de9fdf714b084c6d0258e8319740544cd
abenhansen <abenhansen@gmail.com>
2018-12-14 09:19:59 +0100

    Tilføjet ugeplan

--- src/main/java/vognmand/demo/controllers/OpgaveController.java
+++ src/main/java/vognmand/demo/controllers/OpgaveController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import vognmand.demo.model.Bruger;
 import vognmand.demo.model.BrugerRepository;
@@ -40,6 +41,28 @@ public class OpgaveController {
     public String courseCreate(@ModelAttribute Opgave opgave) {
         opgaverepo.save(opgave);
         return "redirect:/opgaveList";
+    }
+
+    @GetMapping("/opgaveList/sletAlt")
+    public String sletAlt (){
+        opgaverepo.deleteAll();;
+        return "redirect:/opgaveList";
+    }
+
+    @GetMapping("/opgaveList/redigerOpgave/{id}")
+    public String redigerOpgave(@PathVariable long id, Model model){
+        Opgave opgave = opgaverepo.findById(id);
+        model.addAttribute("brugere", brugerrepo.findAll());
+        model.addAttribute("opgave", opgave);
+        return "redigerOpgave";
+
+    }
+
+    @PostMapping("/opgaveList/redigerOpgave")
+    public String redigerOpgave(@ModelAttribute Opgave opgave ){
+        opgaverepo.save(opgave);
+        return "redirect:/opgaveList";
+
     }
 
 
